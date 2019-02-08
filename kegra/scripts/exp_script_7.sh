8e7c3238f5f73b9628452f9664fbb344763600c1
cx2 <cx2@illinois.edu>
2019-02-08 00:59:53 -0600

    grid search

---
+++ kegra/scripts/exp_script_7.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=272 -lplace=excl
+
+# source /opt/intel/compilers_and_libraries_2017/linux/mpi/bin64/mpivars.sh
+source activate tensorflow_mpi
+
+
+### OPA FABRIC ###
+export I_MPI_FABRICS=ofi
+export I_MPI_TMI_PROVIDER=psm2
+export HFI_NO_CPUAFFINITY=1
+export I_MPI_FALLBACK=0
+export OMP_NUM_THREADS=68
+
+
+### OPA FABRIC ###
+export I_MPI_HYDRA_BOOTSTRAP=rsh
+export I_MPI_HYDRA_BOOTSTRAP_EXEC=pbs_tmrsh
+
+export KMP_AFFINITY=granularity=fine,compact,1,0;
+
+# basename=/homes/cx2/gcn/keras-gcn/results/exp_script_4
+
+# watchfile=$basename.log
+# modelfile=$basename.h5
+
+# cd /homes/cx2/gcn/keras-gcn/kegra/
+# # python train_rgcn.py 2>&1 | tee $watchfile
+# python train_gcn_dg.py --save $modelfile --lr 0.01 --nfolds 0 --augmentation no_augmentation --nepochs 500 --nlayers 2 --selfloop eye --ntrials 10 2>&1 | tee $watchfile
+
+
+basename=/homes/cx2/gcn/keras-gcn/results/gcn_exp
+dataset="cora"
+percent=0.005
+lr=0.03
+
+watchfile1=${basename}_7.log
+
+for nlayers in 1 2 3
+do
+    for expm in 1 2 3
+    do
+        watchfile=${basename}_${dataset}_${percent}_${lr}_${nlayers}_${expm}.log
+        modelfile=${basename}_${dataset}_${percent}_${lr}_${nlayers}_${expm}.h5
+        cd /homes/cx2/gcn/keras-gcn/kegra/
+        python train_gcn_exponential_1.py --save ${modelfile} --dataset ${dataset} --train-percent ${percent} --lr ${lr} --nepochs 200 --nlayers ${nlayers} --nfilters 16 --expm ${expm} --ntrials 10 2>&1 | tee ${watchfile} ${watchfile1}
+    done
+done
